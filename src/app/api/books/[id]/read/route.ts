63a48293dee565fbda83905f43437864b23bede3
Matias Dalto <matud_ntc@hotmail.com>
2025-05-12 10:55:18 -0300

    Version 1.0.4

--- src/app/api/books/[id]/read/route.ts
+++ src/app/api/books/[id]/read/route.ts
@@ -1,13 +1,13 @@
-import { prisma } from "../../../../../../lib/prisma";
-import { NextResponse } from "next/server";
+import { prisma } from '../../../../../../lib/prisma';
+import { NextResponse, NextRequest } from 'next/server';
 
-export async function POST(
-  req: Request,
-  { params }: { params: { id: string } },
-) {
-  const bookId = parseInt(params.id);
-  if (isNaN(bookId))
-    return NextResponse.json({ error: "ID inválido" }, { status: 400 });
+export async function POST(req: NextRequest) {
+  const id = req.nextUrl.pathname.split('/').at(-2); // obtiene el `[id]` desde la URL
+
+  const bookId = parseInt(id || '');
+  if (isNaN(bookId)) {
+    return NextResponse.json({ error: 'ID inválido' }, { status: 400 });
+  }
 
   await prisma.book.update({
     where: { id: bookId },
